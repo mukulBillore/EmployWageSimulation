32ed774dc1547af549345ea553690c1d83d5d694
mukulBillore <mukulbillore88@gmail.com>
2021-12-20 01:35:29 +0530

    Added : presentorAbsent code

--- src/com/bridgelabs/EmployWageSimulation.java
+++ src/com/bridgelabs/EmployWageSimulation.java
@@ -1,9 +1,16 @@
 package com.bridgelabs;
 
+import java.util.Random;
+
 public class EmployWageSimulation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random rand=new Random();
+		int presentOrAbsent=rand.nextInt(2);
+		System.out.print("welcome to the employ wage simulation ");
+     if(presentOrAbsent==0) {
+    	 System.out.println("Employ is present ");
+     }else System.out.println("Employ is absent");
 
 	}
 
