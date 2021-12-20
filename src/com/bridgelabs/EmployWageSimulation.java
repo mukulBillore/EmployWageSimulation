c219952330bbaea4e0f0c05cdc0c44539d8aac4d
mukulBillore <mukulbillore88@gmail.com>
2021-12-21 00:43:38 +0530

    added : added a feature to calculate the employ wage for a month

--- src/com/bridgelabs/EmployWageSimulation.java
+++ src/com/bridgelabs/EmployWageSimulation.java
@@ -7,17 +7,22 @@ public class EmployWageSimulation {
 	public static void main(String[] args) {
 		Random rand=new Random();
 		int fulltimeEmp=1,partTimeEmploy=2;
+		int oneDayWage=20*8;
+		int countDayPresent=0;
+		for(int i=1;i<=20;i++) {
 		int presentOrAbsent=rand.nextInt(2);
+		
 		System.out.print("welcome to the employ wage simulation ");
-     if(presentOrAbsent==0) {
+		if(presentOrAbsent==0) {
     	 //part time or full time 
     	 System.out.println("Employ is present ");
-    	 System.out.print("employ is one day wage is "+20*8);
+    	 System.out.print("employ one day wage is "+oneDayWage);
+    	 countDayPresent++;
      }else System.out.println("Employ is absent");
      if(fulltimeEmp==1) {
     	 System.out.println("employ is full time ");
      }
      else {System.out.println("Employ is part time ");}
+	}System.out.println("The totel wage of a employ for a month is :-"+countDayPresent*oneDayWage);
 	}
-
 }
