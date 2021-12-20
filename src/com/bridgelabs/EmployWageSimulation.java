1bcb0183e39396eaf2e705e252bbd65c8a0c06b2
mukulBillore <mukulbillore88@gmail.com>
2021-12-21 00:59:08 +0530

    added : added class and object functionality

--- src/com/bridgelabs/EmployWageSimulation.java
+++ src/com/bridgelabs/EmployWageSimulation.java
@@ -3,12 +3,11 @@ package com.bridgelabs;
 import java.util.Random;
 
 public class EmployWageSimulation {
-
-	public static void main(String[] args) {
-		Random rand=new Random();
-		int fulltimeEmp=1,partTimeEmploy=2;
-		int oneDayWage=20*8;
-		int countDayPresent=0;
+	Random rand=new Random();
+	int fulltimeEmp=1,partTimeEmploy=2;
+	final int oneDayWage=20*8;
+	int countDayPresent=0;
+	public void wageCalculator() {
 		while(countDayPresent<20) {
 			if(countDayPresent*oneDayWage>=100) {
 				break;
@@ -27,5 +26,10 @@ public class EmployWageSimulation {
      }
      else {System.out.println("Employ is part time ");}
 	}System.out.println("The totel wage of a employ for a month is :-"+countDayPresent*oneDayWage);
+	
+	}
+	public static void main(String[] args) {
+		EmployWageSimulation wage1= new EmployWageSimulation();
+		wage1.wageCalculator();
 	}
 }
