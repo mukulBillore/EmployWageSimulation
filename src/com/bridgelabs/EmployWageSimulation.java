64c5e96ed03df28c0bf8671bb72bdea93b92754e
mukulBillore <mukulbillore88@gmail.com>
2021-12-21 00:33:26 +0530

    Added : added part time employ

--- src/com/bridgelabs/EmployWageSimulation.java
+++ src/com/bridgelabs/EmployWageSimulation.java
@@ -6,13 +6,18 @@ public class EmployWageSimulation {
 
 	public static void main(String[] args) {
 		Random rand=new Random();
+		int fulltimeEmp=1,partTimeEmploy=2;
 		int presentOrAbsent=rand.nextInt(2);
 		System.out.print("welcome to the employ wage simulation ");
      if(presentOrAbsent==0) {
+    	 //part time or full time 
     	 System.out.println("Employ is present ");
     	 System.out.print("employ is one day wage is "+20*8);
      }else System.out.println("Employ is absent");
-
+     if(fulltimeEmp==1) {
+    	 System.out.println("employ is full time ");
+     }
+     else {System.out.println("Employ is part time ");}
 	}
 
 }
